c1e6a427b5ab7a111e41dfa7771eebd763f90593
Zach Coyle <zach.coyle@gmail.com>
2021-02-17 21:00:56 -0500

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -15,16 +15,17 @@
     formatter-nvim = { url = github:mhartington/formatter.nvim; flake = false; };
     fzf-lsp-nvim = { url = github:gfanto/fzf-lsp.nvim; flake = false; };
     galaxyline-nvim = { url = github:glepnir/galaxyline.nvim/main; flake = false; };
+    gruvbox = { url = github:gruvbox-community/gruvbox; flake = false; };
     lspkind-nvim = { url = github:onsails/lspkind-nvim; flake = false; };
     lush-nvim = { url = github:rktjmp/lush.nvim; flake = false; };
     nvim-compe = { url = github:hrsh7th/nvim-compe; flake = false; };
     nvim-lspconfig = { url = github:neovim/nvim-lspconfig; flake = false; };
     scrollbar-nvim = { url = github:Xuyuanp/scrollbar.nvim; flake = false; };
     snippets-nvim = { url = github:norcalli/snippets.nvim; flake = false; };
+    telescope-nvim = { url = github:nvim-telescope/telescope.nvim; flake = false; };
     vim-dadbod-ui = { url = github:kristijanhusak/vim-dadbod-ui; flake = false; };
     vim-prisma = { url = github:pantharshit00/vim-prisma; flake = false; };
     vim-vsnip = { url = github:hrsh7th/vim-vsnip; flake = false; };
-    gruvbox = { url = github:gruvbox-community/gruvbox; flake = false; };
 
     rnix-lsp.url = github:nix-community/rnix-lsp;
 
