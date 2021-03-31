c48b492e08b26bf9ee97221efbbbcc4757c61557
Zach Coyle <zach.coyle@gmail.com>
2021-03-31 00:16:25 -0400

    remove vsnip references

--- defaults/config/nvim-compe-config.lua
+++ defaults/config/nvim-compe-config.lua
@@ -45,8 +45,6 @@ end
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
   elseif check_back_space() then
     return t "<Tab>"
   else
@@ -56,8 +54,6 @@ end
 _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
   else
     return t "<S-Tab>"
   end
