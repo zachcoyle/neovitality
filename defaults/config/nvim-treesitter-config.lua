efb2c8182304fd191aea2b3aa331284615e49896
Zach Coyle <zach.coyle@gmail.com>
2021-07-12 19:54:43 -0400

    format all lua files

--- defaults/config/nvim-treesitter-config.lua
+++ defaults/config/nvim-treesitter-config.lua
@@ -1,29 +1,29 @@
-require('nvim-treesitter.configs').setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-  },
-   autotag = {
-    enable = true,
-  },
-  context_commentstring = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
-}
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+	},
+	autotag = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+})
 
-vim.cmd [[set foldmethod=expr]]
-vim.cmd [[set foldlevel=10]]
-vim.cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
+vim.cmd([[set foldmethod=expr]])
+vim.cmd([[set foldlevel=10]])
+vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
--- defaults/config/telescope-nvim-config.lua
+++ defaults/config/telescope-nvim-config.lua
@@ -1,8 +1,8 @@
-require('telescope').setup{
-    defaults = { 
-        file_ignore_patterns = {
-            "flake.lock", 
-            "yarn.lock",
-        },
-    }
-}
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			"flake.lock",
+			"yarn.lock",
+		},
+	},
+})
