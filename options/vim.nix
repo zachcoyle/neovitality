5dc03e6586b4203aa0dbf9d3635d79a403ea1525
Zach Coyle <zach.coyle@gmail.com>
2021-07-15 15:35:56 -0400

    add efm-langserver for eslint

--- options/vim.nix
+++ options/vim.nix
@@ -224,6 +224,63 @@ in
             local lspconfig = require'lspconfig'
 
             ${builtins.concatStringsSep "\n" lspConfigs}
+            
+            -- temporary but I need it for work --
+
+            local eslint = {
+              lintCommand = "${pkgs.nodePackages.eslint_d}/bin/eslint_d -f unix --stdin --stdin-filename ''${INPUT}",
+              lintStdin = true,
+              lintFormats = {"%f:%l:%c: %m"},
+              lintIgnoreExitCode = true,
+              formatCommand = "${pkgs.nodePackages.eslint_d}/bin/eslint_d --fix-to-stdout --stdin --stdin-filename=''${INPUT}",
+              formatStdin = true
+            }
+            
+            lspconfig.tsserver.setup {
+              on_attach = function(client)
+                if client.config.flags then
+                  client.config.flags.allow_incremental_sync = true
+                end
+                client.resolved_capabilities.document_formatting = false
+                set_lsp_config(client)
+              end
+            }
+            
+            lspconfig.efm.setup {
+              cmd = { "${pkgs.efm-langserver}/bin/efm-langserver" },
+              on_attach = function(client)
+                client.resolved_capabilities.document_formatting = true
+                client.resolved_capabilities.goto_definition = false
+                set_lsp_config(client)
+              end,
+              root_dir = function()
+                if not eslint_config_exists() then
+                  return nil
+                end
+                return vim.fn.getcwd()
+              end,
+              settings = {
+                languages = {
+                  javascript = {eslint},
+                  javascriptreact = {eslint},
+                  ["javascript.jsx"] = {eslint},
+                  typescript = {eslint},
+                  ["typescript.tsx"] = {eslint},
+                  typescriptreact = {eslint}
+                }
+              },
+              filetypes = {
+                "javascript",
+                "javascriptreact",
+                "javascript.jsx",
+                "typescript",
+                "typescript.tsx",
+                "typescriptreact"
+              },
+            }
+
+            --------------------------------------
+
 
             local function preview_location_callback(_, _, result)
               if result == nil or vim.tbl_isempty(result) then
