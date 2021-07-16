21e5c8f5052a279dc1e08989920abf7bbbb62f18
Zach Coyle <zach.coyle@gmail.com>
2021-07-16 08:13:08 -0400

    efm-langserver try 2

--- options/vim.nix
+++ options/vim.nix
@@ -225,6 +225,50 @@ in
 
             ${builtins.concatStringsSep "\n" lspConfigs}
 
+            local lspconfig = require"lspconfig"
+
+            -------  temp --------- 
+            local eslint = {
+              lintCommand = "eslint_d -f unix --stdin --stdin-filename ''${INPUT}",
+              lintStdin = true,
+              lintFormats = {"%f:%l:%c: %m"},
+              lintIgnoreExitCode = true,
+              formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=''${INPUT}",
+              formatStdin = true
+            }
+            
+            lspconfig.efm.setup {
+              on_attach = function(client)
+                client.resolved_capabilities.document_formatting = true
+                client.resolved_capabilities.goto_definition = false
+                set_lsp_config(client)
+              end,
+              root_dir = function()
+                if not eslint_config_exists() then
+                  return nil
+                end
+                return vim.fn.getcwd()
+              end,
+              settings = {
+                languages = {
+                  javascript = {eslint},
+                  javascriptreact = {eslint},
+                  ["javascript.jsx"] = {eslint},
+                  typescript = {eslint},
+                  ["typescript.tsx"] = {eslint},
+                  typescriptreact = {eslint}
+                }
+              },
+              filetypes = {
+                "javascript",
+                "javascriptreact",
+                "javascript.jsx",
+                "typescript",
+                "typescript.tsx",
+                "typescriptreact"
+              },
+            }
+            ------------------
             local function preview_location_callback(_, _, result)
               if result == nil or vim.tbl_isempty(result) then
                 return nil
