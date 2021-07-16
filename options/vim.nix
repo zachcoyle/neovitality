f87b56015b5e6cdb57722f3d6622067540cac42e
Zach Coyle <zach.coyle@gmail.com>
2021-07-16 08:29:53 -0400

    efm try 3

--- options/vim.nix
+++ options/vim.nix
@@ -228,6 +228,21 @@ in
             local lspconfig = require"lspconfig"
 
             -------  temp --------- 
+            local function eslint_config_exists()
+              local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+          
+              if not vim.tbl_isempty(eslintrc) then
+                return true
+              end
+          
+              if vim.fn.filereadable("package.json") then
+                if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+                  return true
+                end
+              end
+          
+              return false
+            end
             local eslint = {
               lintCommand = "eslint_d -f unix --stdin --stdin-filename ''${INPUT}",
               lintStdin = true,
